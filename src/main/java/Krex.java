109329c701e514119b2cc5dad1b6f4e1ddb38c30
Kairav Shaurya Makhija <krex8@MacBook-Pro-2.local>
2026-01-21 01:20:45 +0800

    feat: Level-1 add echo loop and bye command

--- src/main/java/Krex.java
+++ src/main/java/Krex.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Krex {
     public static void main(String[] args) {
         Ui.showWelcome();
-        Ui.showBye();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.trim().equals("bye")) {
+                Ui.showBye();
+                break;
+            }
+
+            Ui.showMessage(input);
+        }
     }
 }
